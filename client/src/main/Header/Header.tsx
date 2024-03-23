876d42e89c1e7b2ed833bc64fdd37d608f53d440
Bruno DaSilva <hi.dasilvab@gmail.com>
2024-03-22 21:53:53 -0500

    added api and vs debug session

--- client/src/main/Header/Header.tsx
+++ client/src/main/Header/Header.tsx
@@ -1,12 +1,17 @@
 import logo from "./LIH-MODERN-LOGO.png";
 import "./Header.css";
 
-const Header = () => {
+type Header = {
+  subtitle: string;
+};
+
+const Header = ({ subtitle }: Header) => {
   return (
     <header className="header">
       <div>
         <img src={logo} className="header__logo" alt="LIH-MODERN-LOGO" />
       </div>
+      <div>{subtitle}</div>
     </header>
   );
 };
